03fa94cc97f6060c7652cda535cb668bd7ab24b1
CarassiusPro <carassius@gmail.com>
2016-01-09 20:07:17 +0800

    Update toggle_field.php
    Updated to fix a bug with needing to change the ID's
    Added in support for 3 toggles
    Added support to add non active color
    Enqueued css so not loading the css for each field

--- toggle_field.php
+++ toggle_field.php
@@ -3,75 +3,13 @@
 function fl_toggle_field_true($name, $value, $field) {
    
 ?>
-<style>
-.switch-field {
-  font-family: "Lucida Grande", Tahoma, Verdana, sans-serif;
-
-    overflow: hidden;
-}
-
-.switch-title {
-  margin-bottom: 6px;
-}
-
-.switch-field input {
-  display: none;
-}
-
-.switch-field label {
-  float: left;
-}
-
-.switch-field label {
-  display: inline-block;
-  width: 60px;
-  background-color: #e4e4e4;
-  color: rgba(0, 0, 0, 0.6);
-  font-size: 14px;
-  font-weight: normal;
-  text-align: center;
-  text-shadow: none;
-  padding: 2px;
-  border: 1px solid rgba(0, 0, 0, 0.2);
-  -webkit-box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.3), 0 1px rgba(255, 255, 255, 0.1);
-  box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.3), 0 1px rgba(255, 255, 255, 0.1);
-  -webkit-transition: all 0.1s ease-in-out;
-  -moz-transition:    all 0.1s ease-in-out;
-  -ms-transition:     all 0.1s ease-in-out;
-  -o-transition:      all 0.1s ease-in-out;
-  transition:         all 0.1s ease-in-out;
-}
-
-.switch-field label:hover {
-    cursor: pointer;
-}
-
-.switch-field input:checked + label {
-    <?php 
-    if (array_key_exists('color', $field)) {
-       $color = $field['color'];
-   } else {
-       $color = '#A5DC86';
-   }
-    ?>
-  background-color: <?php echo $color; ?>;
-  -webkit-box-shadow: none;
-  box-shadow: none;
-}
-
-.switch-field label:first-of-type {
-  border-radius: 4px 0 0 4px;
-}
-
-.switch-field label:last-of-type {
-  border-radius: 0 4px 4px 0;
-}
-</style>
 
 <div class="switch-field">
 
 <?php
-
+    
+    $buttoncount = count($field['options']);
+    
     if (array_key_exists('options', $field)) {
         
         $options = $field['options'];
@@ -85,25 +23,60 @@ function fl_toggle_field_true($name, $value, $field) {
         $leftValue = $options[$leftKey];
         
         $rightValue = $options[$rightKey];	
+        
+        if($buttoncount == 3) {
+            
+            $lastKey = $keys[2];
+            
+            $lastValue = $options[$lastKey];
+            
+        }
 	
     } else {
         
-        $leftKey = 'true';
+        $leftKey = 'yes';
         
-        $rightKey = 'false';
+        $rightKey = 'no';
         
-        $leftValue = 'True';
+        $leftValue = 'Yes';
         
-        $rightValue = 'False';
+        $rightValue = 'No';
         
     }
 ?>
-
-      <input type="radio" id="switch_left" name="<?php echo $name; ?>" value="<?php echo $leftKey; ?>" <?php if (isset($value) && $value==$leftKey) echo "checked"; ?> />
-      <label for="switch_left"><?php echo $leftValue; ?></label>
+<style>
+.switch-field .<?php echo $name; ?> {
+    <?php 
+    if (array_key_exists('offcolor', $field)) {
+       $color = $field['offcolor'];
+   } else {
+       $color = '#e4e4e4';
+   }
+    ?>
+  background-color: <?php echo $color; ?>;
+    }
+.switch-field  .<?php echo $name; ?>:checked + .<?php echo $name; ?> {
+    <?php 
+    if (array_key_exists('oncolor', $field)) {
+       $color = $field['oncolor'];
+   } else {
+       $color = '#A5DC86';
+   }
+    ?>
+  background-color: <?php echo $color; ?>;
+}
+</style>
     
-      <input type="radio" id="switch_right" name="<?php echo $name; ?>" value="<?php echo $rightKey; ?>" <?php if (isset($value) && $value==$rightKey) echo "checked"; ?> />
-      <label for="switch_right"><?php echo $rightValue; ?></label>
+      <input type="radio" class="<?php echo $name; ?>" id="<?php echo $name; ?>_left" name="<?php echo $name; ?>" value="<?php echo $leftKey; ?>" <?php if (isset($value) && $value==$leftKey) echo "checked"; ?> />
+      <label class="<?php echo $name; ?>" for="<?php echo $name; ?>_left"><?php echo $leftValue; ?></label>
+    
+      <input type="radio" class="<?php echo $name; ?>" id="<?php echo $name; ?>_right" name="<?php echo $name; ?>" value="<?php echo $rightKey; ?>" <?php if (isset($value) && $value==$rightKey) echo "checked"; ?> />
+      <label class="<?php echo $name; ?>" for="<?php echo $name; ?>_right"><?php echo $rightValue; ?></label>   
+    
+    <?php if($buttoncount == 3) { ?>
+    <input type="radio" class="<?php echo $name; ?>" id="<?php echo $name; ?>_last" name="<?php echo $name; ?>" value="<?php echo $lastKey; ?>" <?php if (isset($value) && $value==$lastKey) echo "checked"; ?> />
+      <label class="<?php echo $name; ?>" for="<?php echo $name; ?>_last"><?php echo $lastValue; ?></label>
+    <?php } ?>
     
     </div>
 <?php
@@ -111,3 +84,8 @@ function fl_toggle_field_true($name, $value, $field) {
 }
 
 add_action('fl_builder_control_toggle', 'fl_toggle_field_true', 1, 3);
+
+function sw_enqueue_toggle() {
+	wp_enqueue_style( 'toggle-css', plugin_dir_url( __FILE__ ) . 'toggle.css' );
+}
+add_action( 'wp_enqueue_scripts', 'sw_enqueue_toggle' );
